408addfb2fd3865267c790da70c64f36865c28f6
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2018-02-25 19:25:31 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,5 +3,5 @@
 
 consumer_key= 'RmN7VPJii5SrYTPLiUNsExIhn'
 consumer_secret = '8tsV2ttUtDKtYNOkTuILbZnt8gtByypqZaaOAzRcovNNoYEM7k'
-access_token = '966495937987338240-W0KQ4zpOTH9AVCajo5e7ARc7h3IxQgJ'
-access_token_secret = 'rN1EwnAwGq6oTJ515i3nyyWoJuSmSf95TsyuB5CqyTJqI'
+access_token = '966495937987338240-yIZoKIsEGLucpi1qDDRsD2eW1IkWwNm'
+access_token_secret = '	vzGuXBTzFw8S5Wtbo95zMV9tjn6T9uRiLXkeeMEKPfDto'
