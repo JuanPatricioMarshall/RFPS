47e126da2c32a401091468dc6fc501cdd921c9c2
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2018-03-07 18:40:37 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,5 +3,5 @@
 
 consumer_key= 'RmN7VPJii5SrYTPLiUNsExIhn'
 consumer_secret = '8tsV2ttUtDKtYNOkTuILbZnt8gtByypqZaaOAzRcovNNoYEM7k'
-access_token = '966495937987338240-yIZoKIsEGLucpi1qDDRsD2eW1IkWwNm'
-access_token_secret = 'vzGuXBTzFw8S5Wtbo95zMV9tjn6T9uRiLXkeeMEKPfDto'
+access_token = '966495937987338240-jLLB2siXLDOUBJ1U8Z5dODI2PA8a70X'
+access_token_secret = '	zsZDk1CjKRhj6mD6D9S7vZa1vrzhXkliMSDyK76ZfDtwl'
