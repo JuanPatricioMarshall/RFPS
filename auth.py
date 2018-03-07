7afb2c06d1cd7865a0610ecc9cd15576ea744f5f
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2018-03-07 18:44:09 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -4,4 +4,4 @@
 consumer_key= 'RmN7VPJii5SrYTPLiUNsExIhn'
 consumer_secret = '8tsV2ttUtDKtYNOkTuILbZnt8gtByypqZaaOAzRcovNNoYEM7k'
 access_token = '966495937987338240-jLLB2siXLDOUBJ1U8Z5dODI2PA8a70X'
-access_token_secret = '	zsZDk1CjKRhj6mD6D9S7vZa1vrzhXkliMSDyK76ZfDtwl'
+access_token_secret = 'zsZDk1CjKRhj6mD6D9S7vZa1vrzhXkliMSDyK76ZfDtwl'
